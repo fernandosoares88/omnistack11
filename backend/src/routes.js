318ffb1d1fa7162d880f6cd0552a5ba100a5f585
Fernando Soares <fernandolinharessoares@gmail.com>
2020-03-24 18:08:25 -0300

    Criando as rotas para buscar a lista de ongs e para adicionar uma nova ong

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,34 @@
 const express = require('express')
+const crypto = require('crypto')
+const connection = require('./database/connection')
 
 const routes = express.Router()
 
-routes.post('/', (request, response) => {
-    
-    return response.json({
-        evento: 'semana omni stack 11.0',
-        aluno: 'Fernando Soares'
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*')
+
+    return response.json(ongs)
+})
+
+/**
+ * Rota para criar uma nova ONG
+ */
+routes.post('/ongs', async (request, response) => {
+    const { nome, email, whatsapp, city, uf } = request.body
+    // console.log(data)
+
+    const id = crypto.randomBytes(4).toString('HEX')
+
+    await connection('ongs').insert({
+        id, 
+        nome,
+        email,
+        whatsapp,
+        city,
+        uf
     })
+
+    return response.json({id})
 })
 
 module.exports = routes
