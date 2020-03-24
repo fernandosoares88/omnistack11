cccfd20c726467c86d30b776188a115d8efd1a32
Fernando Soares <fernandolinharessoares@gmail.com>
2020-03-24 18:19:17 -0300

    Modularizando as regras de negócio de ongs em um arquivo OngController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,34 +1,16 @@
 const express = require('express')
-const crypto = require('crypto')
-const connection = require('./database/connection')
+const OngController = require('./controllers/OngController')
 
 const routes = express.Router()
 
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*')
-
-    return response.json(ongs)
-})
+/**
+ * Rota para recuperar a lista de ONGs
+ */
+routes.get('/ongs', OngController.list)
 
 /**
  * Rota para criar uma nova ONG
  */
-routes.post('/ongs', async (request, response) => {
-    const { nome, email, whatsapp, city, uf } = request.body
-    // console.log(data)
-
-    const id = crypto.randomBytes(4).toString('HEX')
-
-    await connection('ongs').insert({
-        id, 
-        nome,
-        email,
-        whatsapp,
-        city,
-        uf
-    })
-
-    return response.json({id})
-})
+routes.post('/ongs', OngController.create)
 
 module.exports = routes
