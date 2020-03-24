e548cf9d70c0e5bda9dd466fc8c57850d3678b73
Fernando Soares <fernandolinharessoares@gmail.com>
2020-03-24 20:41:49 -0300

    Criando funcionalidade de retorno dos Incidents de uma Ong

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -3,7 +3,6 @@ const connection = require('../database/connection')
 
 module.exports = {
     async index(request, response) {
-        const ong_id = request.headers.authorization
         const incidents = await connection('incidents').select('*')
 
         return response.json(incidents)
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
 
 const routes = express.Router()
 
@@ -10,6 +11,10 @@ const routes = express.Router()
 routes.get('/ongs', OngController.index)
 routes.post('/ongs', OngController.create)
 
+/**
+ * Rotas para Profile
+ */
+routes.get('/profile', ProfileController.index)
 
 /**
  * Rotas para casos (incidents)
