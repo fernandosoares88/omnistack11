3e313c525beeb24bfb90fc9154e79725fc3c07bf
Fernando Soares <fernandolinharessoares@gmail.com>
2020-03-24 20:28:21 -0300

    Criando funcionalidade de remoção de um Incident

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -20,5 +20,23 @@ module.exports = {
         })
 
         return response.json({ id })
-    }
+    },
+    async delete(request, response) {
+        const ong_id = request.headers.authorization
+        const { id } = request.params
+
+        const incident = await connection('incidents')
+            .where('id', id)
+            .select('ong_id')
+            .first()
+
+        //Falta tratar o caso do id do incidente não existir no banco
+        if (incident.ong_id !== ong_id) {
+            return response.status(401).json({ error: 'Operation not permitted' })
+        }
+
+        await connection('incidents').where('id', id).delete()
+
+        return response.status(204).send()
+    },
 }
