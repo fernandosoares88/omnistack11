b3dfd77236f20d4df17b399a848edec71f3d9bdf
Fernando Soares <fernandolinharessoares@gmail.com>
2020-03-24 21:50:24 -0300

    Configurando o CORS na aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
-
 const routes = require('./routes') // ./ é necessário pois é um arquivo e não um módulo
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
+
+// Em produção seria algo mais ou menos assim:
+// app.use(cors({
+//     origin: 'https://meuapp.com'
+// }))
 
 /**
  * Utilizando o JSON no body das requisições
