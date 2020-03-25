ab6901edb7bb083f6742d859124735c64dd21a23
Fernando Soares <fernandolinharessoares@gmail.com>
2020-03-25 19:53:46 -0300

    Criando página de Login e fazendo configurações de CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,15 @@
 import React, { useState } from 'react';
 
-import Header from './Header'
+import './global.css'
+
+import Logon from './pages/Logon'
 
 function App() {
 
-  const [counter, setCounter] = useState(0)
-
-  function increment() {
-    setCounter(counter+1)
-  }
 
   return (
     <div>
-      <Header>
-        Contador: {counter}
-      </Header>
-      <button onClick={increment}>Incrementar</button>
+      <Logon />
     </div>
   );
 }
