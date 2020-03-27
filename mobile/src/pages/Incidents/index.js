7be366c8c567944ad39053be64b35af54c3b0e67
Fernando Soares <fernandolinharessoares@gmail.com>
2020-03-26 23:20:17 -0300

    Scroll infinito, finalizando a aplicação

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -11,12 +11,29 @@ import api from '../../services/api'
 export default function Incidents() {
     const [incidents, setIncidents] = useState([])
     const [total, setTotal] = useState(0)
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
+
     const navigation = useNavigation()
 
     async function loadIncidents() {
-        const response = await api.get('/incidents')
-        setIncidents(response.data)
+        if (loading) {
+            return
+        }
+
+        if (total > 0 && incidents.length === total) {
+            return
+        }
+
+        setLoading(true)
+
+        const response = await api.get('/incidents', {
+            params: { page }
+        })
+        setIncidents([...incidents, ...response.data])
         setTotal(response.headers['x-total-count'])
+        setPage(page + 1)
+        setLoading(false)
     }
 
     useEffect(() => {
@@ -42,6 +59,8 @@ export default function Incidents() {
                 data={incidents}
                 keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
+                onEndReached={loadIncidents}
+                onEndReachedThreshold={0.2}
                 renderItem={({ item: incident }) => (
                     <View style={styles.incident}>
                         <Text style={styles.incidentProperty}>ONG:</Text>
